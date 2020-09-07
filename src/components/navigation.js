f8e0177e7f8986d486ce2fc6cf98c685d197c183
Moody Rahman <mrahman01@stuy.edu>
2020-09-07 13:26:11 -0400

    removed theme changer

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,15 +5,10 @@ import ThemeChanger from "../components/themeChanger"
 export default (props) => (
   <nav className="navigation"> 
     <Link to="https://hunterosc.org/">OSC</Link>
-    <ThemeChanger/>
     <Link to="https://www.hunteracm.org/">ACM</Link>
-    <ThemeChanger/>
     <Link to="http://dsc.huntercs.club">DSC</Link>
-    <ThemeChanger/>
     <Link to="http://discord.huntercs.club">Hunter CS Network Discord</Link>
-    <ThemeChanger/>
     <Link to="http://blog.huntercs.club">Our Medium</Link>
-    <ThemeChanger/>
   </nav>
   
 )
